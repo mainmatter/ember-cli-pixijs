39b56bb03b3b5eea25bfcc2d3e49756f8f6a9722
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2016-02-10 16:26:32 +0100

    wrap pixi.js

--- index.js
+++ index.js
@@ -2,5 +2,14 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-pixijs'
+  name: 'ember-cli-pixijs',
+
+  init: function(name) {
+    var assetsPath = require('path').join('src', 'index.js');
+    this.treePaths['vendor'] = require.resolve('pixi.js').replace(assetsPath, '');
+  },
+
+  included: function(app) {
+    this.app.import('vendor/bin/pixi.js');
+  }
 };
