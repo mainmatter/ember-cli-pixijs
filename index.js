d8af824ecce671e86e25725f046ee1ba47883b77
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2016-02-11 14:58:14 +0100

    cleanup

--- index.js
+++ index.js
@@ -5,30 +5,24 @@ var Funnel = require('broccoli-funnel');
 var mergeTrees = require('broccoli-merge-trees');
 var path = require('path');
 
-var instrument = require('broccoli-debug').instrument;
-
 module.exports = {
   name: 'ember-cli-pixijs',
 
   included: function(app) {
-    var importPaths = [];
+    var vendorPath = this.treePaths.vendor;
 
     if (app.env === 'test') {
-      importPaths.push(path.join('es5-shim', 'es5-shim.js'));
+      app.import(path.join(vendorPath, 'es5-shim', 'es5-shim.js'));
     }
 
     if (app.env === 'production') {
-      importPaths.push(path.join('pixi.js', 'pixi.min.js'));
+      app.import(path.join(vendorPath, 'pixi.js', 'pixi.min.js'));
     } else {
-      importPaths.push(path.join('pixi.js', 'pixi.js'));
-      importPaths.push(path.join('pixi.js', 'pixi.js.map'));
+      app.import(path.join(vendorPath, 'pixi.js', 'pixi.js'));
+      app.import(path.join(vendorPath, 'pixi.js', 'pixi.js.map'));
     }
 
-    importPaths.push(path.join('pixi-shim.js'));
-
-    for (var importPath of importPaths) {
-      app.import(path.join(this.treePaths.vendor, importPath));
-    }
+    app.import(path.join(vendorPath, 'pixi-shim.js'));
   },
 
   treeForVendor: function(vendorTree) {
@@ -51,6 +45,6 @@ module.exports = {
       exclude: ['tests/**/*']
     }));
     
-    return instrument.print(mergeTrees(trees));
+    return mergeTrees(trees);
   }
 };
