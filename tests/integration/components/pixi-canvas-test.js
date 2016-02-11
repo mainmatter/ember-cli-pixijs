58abb22e2098bd77a71bdd39c9f806c5710e5578
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2016-02-11 11:56:03 +0100

    add test for pixi-canvas component

---
+++ tests/integration/components/pixi-canvas-test.js
@@ -0,0 +1,50 @@
+/* jshint expr:true */
+import { expect } from 'chai';
+import { describeComponent, it } from 'ember-mocha';
+import hbs from 'htmlbars-inline-precompile';
+
+describeComponent('pixi-canvas', 'Integration: PixiCanvasComponent', { integration: true }, () => {
+  it('renders a canvas element', function() {
+    this.render(hbs`
+      {{pixi-canvas}}
+    `);
+
+    const $canvas = this.$('canvas');
+
+    expect($canvas).to.be.ok;
+  });
+
+  it('renders a canvas element with the specified size', function() {
+    this.render(hbs`
+      {{pixi-canvas width=200 height=100}}
+    `);
+
+    const $canvas = this.$('canvas');
+    const width = Number($canvas.attr('width'));
+    const height = Number($canvas.attr('height'));
+
+    expect(width).to.eq(200);
+    expect(height).to.eq(100);
+  });
+
+  it('replaces the canvas element when the dimensions change', function() {
+    this.set('width', 200);
+    this.set('height', 100);
+    this.render(hbs`
+      {{pixi-canvas width=width height=height}}
+    `);
+
+    const $oldCanvas = this.$('canvas');
+
+    this.set('width', 100);
+    this.set('height', 50);
+
+    const $newCanvas = this.$('canvas');
+    const width = Number($newCanvas.attr('width'));
+    const height = Number($newCanvas.attr('height'));
+
+    expect($oldCanvas).to.not.eql($newCanvas);
+    expect(width).to.eq(100);
+    expect(height).to.eq(50);
+  });
+});
