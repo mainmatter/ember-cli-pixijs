86c2fa2d4a97960886f17c9425e9eefb30ec101d
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-02-01 11:34:20 +0100

    Replace JSHint/JSCS with ESLint
    and eslint-config-simplabs

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,25 +1,11 @@
-/*jshint node:true*/
-/* global require, module */
-var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
-var path = require('path');
+'use strict';
+
+/* eslint-env node */
+
+const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  var app = new EmberAddon(defaults, {
-    jscsOptions: {
-      enabled: true,
-      testGenerator: function(relativePath, errors) {
-        if (errors) {
-          errors = "\\n" + this.escapeErrorString(errors);
-        } else {
-          errors = "";
-        }
-
-        return "describe('JSCS - " + relativePath + "', function() {\n" +
-          "it('should pass jscs', function() { \n" +
-          "  expect(" + !errors + ", '" + relativePath + " should pass jscs." + errors + "').to.be.ok; \n" +
-          "})});\n";
-      }
-    }
+  let app = new EmberAddon(defaults, {
   });
 
   return app.toTree();
