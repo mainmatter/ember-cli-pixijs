f2986ef2d4e52ce49a88e93a283eae0b77698823
Chris Manson <chris.manson@simplabs.com>
2018-08-02 15:18:48 +0100

    upgrading tests system

--- tests/integration/components/pixi-canvas-test.js
+++ tests/integration/components/pixi-canvas-test.js
@@ -1,52 +1,56 @@
-/* jshint expr:true */
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
-import { setupComponentTest } from 'ember-mocha';
+import { setupRenderingTest } from 'ember-mocha';
+import { render, pauseTest } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-describe('Integration: PixiCanvasComponent', () => {
-  setupComponentTest('pixi-canvas', { integration: true });
+describe('Integration: PixiCanvasComponent', function() {
+  setupRenderingTest();
 
-  it('renders a canvas element', function() {
-    this.render(hbs`
+  it('renders a canvas element', async function() {
+    await render(hbs`
       {{pixi-canvas}}
     `);
 
-    let $canvas = this.$('canvas');
+    // debugger
+    // await pauseTest();
 
-    expect($canvas).to.be.ok;
+    let canvas = this.element.querySelector('canvas');
+
+    expect(canvas).to.be.ok;
   });
 
-  it('renders a canvas element with the specified size', function() {
-    this.render(hbs`
+  it('renders a canvas element with the specified size', async function() {
+    await render(hbs`
       {{pixi-canvas width=200 height=100}}
     `);
 
-    let $canvas = this.$('canvas');
-    let width = Number($canvas.attr('width'));
-    let height = Number($canvas.attr('height'));
+    let canvas = this.element.querySelector('canvas');
+
+    let width = Number(canvas.width);
+    let height = Number(canvas.height);
 
     expect(width).to.eq(200);
     expect(height).to.eq(100);
   });
 
-  it('replaces the canvas element when the dimensions change', function() {
+  it('replaces the canvas element when the dimensions change', async function() {
     this.set('width', 200);
     this.set('height', 100);
-    this.render(hbs`
+    await render(hbs`
       {{pixi-canvas width=width height=height}}
     `);
 
-    let $oldCanvas = this.$('canvas');
+    let oldCanvas = this.element.querySelector('canvas');
 
     this.set('width', 100);
     this.set('height', 50);
 
-    let $newCanvas = this.$('canvas');
-    let width = Number($newCanvas.attr('width'));
-    let height = Number($newCanvas.attr('height'));
+    let newCanvas = this.element.querySelector('canvas');
+    let width = Number(newCanvas.width);
+    let height = Number(newCanvas.height);
 
-    expect($oldCanvas).to.not.eql($newCanvas);
+    expect(oldCanvas).to.not.eql(newCanvas);
     expect(width).to.eq(100);
     expect(height).to.eq(50);
   });
