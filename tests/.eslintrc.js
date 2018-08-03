8f978c0190bfdf528cbe2c88e43eeb6904e35a69
Chris Manson <mansona@users.noreply.github.com>
2018-08-03 11:13:24 +0100

    Upgrade Dependencies (#76)
    * using ember-cli-update to reset some parts of the addon
    
    * running codemods
    
    * upgrading tests system
    
    * fixing tests
    
    * master only for travis
    
    * updating yarn lock
    
    * updating lint command
    
    * fixing regenerator runtime error
    
    * dropping Ember 1.13 because of test system

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
+  env: {
+    node: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+  },
   extends: [
     'simplabs/configs/ember-mocha',
     'simplabs/plugins/mocha',
